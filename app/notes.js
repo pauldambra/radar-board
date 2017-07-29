ca166556c2900c01a5703c4443cec8f86a5160ee
pauldambra <pauldambra@gmail.com>
2017-07-29 11:02:45 +0100

    do the calculations fewer times when drawing notes

--- app/notes.js
+++ app/notes.js
@@ -45,41 +45,49 @@ const wrapLines = (context, textMaxWidth, font, text) => {
   return splitTextToMaxWidth(text, context, textMaxWidth);
 };
 
-// ARGH! all of this calculation could be done once!
-const drawNote = (context, note) => {
-    
-  const wrappedLines = wrapLines(context, paint.desiredNoteWidth, paint.font, note.title);
-
-  const textHeight = wrappedLines.length * (paint.fontHeight + paint.lineSpace) + (paint.textPadding * 2);
-
-  if (textHeight > paint.maxNoteHeight) {
-    //what should we TODO?
-    const errorParams = {
-      wrappedLines, 
-      textHeight, 
-      maxHeight: paint.maxNoteHeight
-    };
-    const message = `wanted max height of ${paint.maxNoteHeight} but wrapped text height is ${textHeight}`;
-    console.error(errorParams, message);
-  }
-
-  const rectangleHeight = textHeight + (paint.textPadding * 2);
+const drawRectangle = (context, note) => {
+  const rectangleHeight = note.textHeight + (paint.textPadding * 2);
   context.fillStyle = paint.fillColour;
   context.fillRect(note.x, note.y, paint.desiredNoteWidth, rectangleHeight);
+}
 
+const drawTitle = (context, note) => {
   context.fillStyle = paint.textColour;
-  wrappedLines.forEach((line, index) => {
+  context.font = paint.font;
+  context.textBaseline = "middle";
+
+  note.wrappedLines.forEach((line, index) => {
     const thisLineOffset = (paint.fontHeight + paint.lineSpace) * index;
     const lineY = (note.y + paint.textPadding) + thisLineOffset;
     const lineWidth = context.measureText(line).width;
     const margin = (paint.desiredNoteWidth - lineWidth) / 2;
     const lineX = note.x + margin;
 
-    context.textBaseline = "middle";
     context.fillText(line, lineX, lineY);
   });
-  context.fillStyle = paint.fillColour;
+}
 
+// ARGH! all of this calculation could be done once!
+const drawNote = (context, note) => {
+    
+  if (!note.wrappedLines) {
+    note.wrappedLines = wrapLines(context, paint.desiredNoteWidth, paint.font, note.title);
+    note.textHeight = note.wrappedLines.length * (paint.fontHeight + paint.lineSpace) + (paint.textPadding * 2);
+  }
+
+  if (note.textHeight > paint.maxNoteHeight) {
+    //what should we TODO?
+    const errorParams = {
+      wrappedLines: note.wrappedLines, 
+      textHeight: note.textHeight, 
+      maxHeight: paint.maxNoteHeight
+    };
+    const message = `wanted max height of ${paint.maxNoteHeight} but wrapped text height is ${note.textHeight}`;
+    console.error(errorParams, message);
+  }
+
+  drawRectangle(context, note);
+  drawTitle(context, note);
 };
 
 const drawAll = context => {
