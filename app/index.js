ed48f17e0b2bfbc0a2eb9a820d0b477ec3d8c865
pauldambra <pauldambra@gmail.com>
2017-07-29 09:35:14 +0100

    close add note overlay on save

--- app/index.js
+++ app/index.js
@@ -19,22 +19,5 @@ window.onload = () => {
   dragger.init(canvas, draw);
   draw();
 
-  const overlayElement = document.getElementById('overlay');
-  const overlayCloser = document.getElementById('overlay-close');  
-  const addNoteButton = document.querySelector('.note-controls [data-add-note]');
-
-  const addNotes$ = rx.Observable.fromEvent(addNoteButton, 'click');
-  addNotes$.subscribe(clicks => overlay.show(overlayElement));
-
-  const closeOverlay$ = rx.Observable.fromEvent(overlayCloser, 'click');
-  closeOverlay$.subscribe(clicks => overlay.hide(overlayElement));
-
-  const saveNoteButton = document.querySelector('[data-save-note]');
-  const saveClicks$ = rx.Observable.fromEvent(saveNoteButton, 'click');
-  saveClicks$.subscribe(clicks => notes.add({
-    context,
-    top: 50,
-    left: 50,
-    title: document.querySelector('[data-note-name]').value
-  }));
+  overlay.init(context);
 }
--- app/overlay.js
+++ app/overlay.js
@@ -1,4 +1,7 @@
 
+const rx = require("rxjs");
+const notes = require('./notes.js');
+
 const show = (overlayElement) => {
   overlayElement.style.visibility = "visible";
 }
@@ -7,7 +10,42 @@ const hide = (overlayElement) => {
   overlayElement.style.visibility = "hidden";
 }
 
+const wireUpOverlayClosing = (overlayElement) => {
+  const overlayCloser = document.getElementById('overlay-close'); 
+  const closeOverlayClicks$ = rx.Observable.fromEvent(overlayCloser, 'click');
+  const closeOverlayOnSave$ = new rx.Subject();
+  const shouldCloseOverlay$ = closeOverlayClicks$.merge(closeOverlayOnSave$);
+  shouldCloseOverlay$.subscribe(clicks => hide(overlayElement));
+
+  return closeOverlayOnSave$;
+}
+
+const init = (context) => {
+  const overlayElement = document.getElementById('overlay');
+   
+  const addNoteButton = document.querySelector('.note-controls [data-add-note]');
+  const saveNoteButton = document.querySelector('[data-save-note]');
+  
+  const addNotes$ = rx.Observable.fromEvent(addNoteButton, 'click');
+  addNotes$.subscribe(clicks => show(overlayElement));
+
+  const manuallyCloseOverlay$ = wireUpOverlayClosing(overlayElement);
+
+  const saveClicks$ = rx.Observable.fromEvent(saveNoteButton, 'click');
+
+  saveClicks$.subscribe(clicks => {
+      notes.add({
+      context,
+      top: 50,
+      left: 50,
+      title: document.querySelector('[data-note-name]').value
+    });
+    manuallyCloseOverlay$.next();
+  });
+}
+
 module.exports = {
+  init,
   show,
   hide
 }
